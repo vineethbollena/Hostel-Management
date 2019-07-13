191736c1dcaa832098f778ef972aa3b77257c5cf
Lenovo <Lenovo@192.168.0.6>
2019-07-13 21:40:44 +0530

    completed

--- src/main/java/main/Hostel.java
+++ src/main/java/main/Hostel.java
@@ -36,21 +36,33 @@ public class Hostel{
 	               String line = null;
 	            
 	               //write your code here !!!
-	               while ((line = br.readLine()) != null) {
+	               while( (line = br.readLine()) != null)
+	               {
 		               String[] splited = line.split("\\s+");
-		               String checkName = splited[0];
+		               String checkName = name;
 		               //write your code here !!!
 //		               compare check name with name and return true if present and false if not
+		               if(splited[0].equals(name)) 
+		               {
+		            	   return true;
+		               }
 	               }
 	               
 	               
 	            }catch(Exception e){
 	                System.out.println(e);
 	            }
-			return true;
+			return false;
 	   }
        public static void allotHostel(){
     	   //write your code here!!!
+    	   try
+    	   {
+    		   writedata();
+    	   }catch(IOException e)
+    	   {
+    		   System.out.println(e);
+    	   }
     	   
        }
 
@@ -76,7 +88,7 @@ public class Hostel{
     	   boolean chk = true;
     	   
     	   //write your code here
-    	   
+    	   chk = readData(name);
     	   return chk;
         }
         
